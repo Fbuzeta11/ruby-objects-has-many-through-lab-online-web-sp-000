de550592ba36973b7db6e02be5583c94c4f619d2
Federico Buzeta Borquez <fbuzeta11@gmail.com>
2020-03-24 00:11:56 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -3,7 +3,7 @@ attr_reader :patient, :doctor, :date
 
   @@all = []
 
-  def initialize(date, doctor, patient)
+  def initialize(date, patient, doctor)
     @patient = patient
     @doctor = doctor
     @date = date
